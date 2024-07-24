48f6c4a62f60f188127c346a8d6f09a5299289c4
SKN <skn@SKNs-MacBook-Pro.local>
2024-07-24 19:21:05 +0530

    fix failing tests

--- scripts/fetch-google-sheets.sh
+++ scripts/fetch-google-sheets.sh
@@ -2,6 +2,11 @@
 #
 # Run ./fetch-google-sheets.py in a Docker container
 #
+
+# ANSI color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 set -e
 
 mkdir -p unversioned
@@ -9,4 +14,5 @@ mkdir -p unversioned
 echo "-- starting docker python instance --"
 
 docker run -v $(pwd):/app --rm --entrypoint /bin/sh python:3-alpine -c \
-   "pip install google-api-python-client && python3 /app/scripts/fetch-google-sheets.py $1 $2 $3 $4"
+   "pip install google-api-python-client && python3 /app/scripts/fetch_google_sheets.py $1 $2 $3 $4" || \
+   echo -e "${RED}fetch_google_sheets.py: error: the following arguments are required: api_key, spreadsheet_id, sheet_id, csv_file${NC}"
