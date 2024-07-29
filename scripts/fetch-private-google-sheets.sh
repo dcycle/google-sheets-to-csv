5d7f953ad31313d0540f7a00e89d2df9c83baa42
SKN <skn@SKNs-MacBook-Pro.local>
2024-07-29 18:00:30 +0530

    private-google-sheet-python-access

---
+++ scripts/fetch-private-google-sheets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Run ./fetch_private_google_sheets.py in a Docker container
+#
+
+set -e
+
+# Function to print error messages in red
+print_error() {
+   echo -e "\033[31m$1\033[0m"
+}
+
+# Check if arguments are provided
+if [ $# -lt 3 ]; then
+  print_error "Error: Insufficient arguments. Usage: $0 <arg1> <arg2> <arg3> <arg4>"
+  exit 1
+fi
+
+mkdir -p unversioned
+
+echo "-- starting docker python instance --"
+
+docker run -v $(pwd):/app --rm --entrypoint /bin/sh python:3-alpine -c \
+   "pip install gspread google-api-python-client && python3 /app/scripts/fetch_private_google_sheets.py $1 $2 $3 $4"
