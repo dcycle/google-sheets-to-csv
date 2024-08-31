7d51c03596e7983d6b98976b83deff4baeb19b8e
Albert Albala <albert@mediatribe.net>
2024-08-31 15:14:17 -0400

    Provide a Jenkins script fo use with a Docker host (#5)

---
+++ scripts/jenkins/fetch-google-sheets.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+scp -r . "$DOCKERHOSTUSER"@"$DOCKERHOST":google-sheets-to-csv
+ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "cd google-sheets-to-csv && ./scripts/fetch-google-sheets.sh $1 $2 $3 $4"
+scp "$DOCKERHOSTUSER"@"$DOCKERHOST":google-sheets-to-csv/unversioned/scripts/data.csv export.csv
