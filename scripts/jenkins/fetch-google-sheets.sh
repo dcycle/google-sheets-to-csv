0ba4be39d863a4452b91811eabe35a7b330f6e6e
Albert Albala <albert@mediatribe.net>
2024-09-01 10:50:10 -0400

    Please squash this commit in the pull request

--- scripts/jenkins/fetch-google-sheets.sh
+++ scripts/jenkins/fetch-google-sheets.sh
@@ -2,5 +2,5 @@
 set -e
 
 scp -r . "$DOCKERHOSTUSER"@"$DOCKERHOST":google-sheets-to-csv
-ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "cd google-sheets-to-csv && ./scripts/fetch-google-sheets.sh $1 $2 $3 $4"
+ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "cd google-sheets-to-csv && ./scripts/fetch-google-sheets.sh $1 $2 $3 ./app/unversioned/scripts/data.csv"
 scp "$DOCKERHOSTUSER"@"$DOCKERHOST":google-sheets-to-csv/unversioned/scripts/data.csv "$CSV_FILENAME"
