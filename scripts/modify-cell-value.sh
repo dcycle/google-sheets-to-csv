ff2ff04db1309ac91d9faeaeaaaa6e0f849bd1b5
SKN <skn@SKNs-MacBook-Pro.local>
2024-12-17 17:39:01 +0530

    modify cel data freeze

---
+++ scripts/modify-cell-value.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Run ./modify_cell_value.py in a Docker container
+#
+
+set -e
+
+# Function to print error messages in red
+print_error() {
+   echo -e "\033[31m$1\033[0m"
+}
+
+# Check if arguments are provided
+if [ $# -lt 4 ]; then
+  print_error "Error: Insufficient arguments. Usage: $0 <arg1> <arg2> <arg3> <arg4>"
+  exit 1
+fi
+
+echo "-- starting docker python instance --"
+
+docker run -v $(pwd):/app --rm --entrypoint /bin/sh python:3-alpine -c \
+   "pip install google-api-python-client google-auth google-auth-oauthlib google-auth-httplib2 && python3 /app/scripts/modify_cell_value.py $1 $2 $3 $4"
